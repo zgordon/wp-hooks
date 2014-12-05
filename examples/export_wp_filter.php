d94c28a00039fbb20e0fdd7f7b46e0d15aa9e9db
Zac Gordon <email@zacgordon.com>
2014-12-05 18:20:43 -0500

    updating slide deck and adding new examples

---
+++ examples/export_wp_filter.php
@@ -0,0 +1,5 @@
+<pre>
+<?php var_export( $wp_filter ); ?>
+</pre>  
+
+<?php// list_hooks(); ?>
--- functions.php
+++ functions.php
@@ -2,12 +2,7 @@
 
 //require_once('debug-hooks.php' );
 
-
-
-function register_my_custom_menu_page(){
-    add_menu_page( 'custom menu title', 'custom menu', 'manage_options', 'myplugin/myplugin-admin.php', '', 'dashicons-admin-site', 6 );
-}
-//require_once( 'examples/wp_filter.php' );
+require_once( 'examples/wp_filter.php' );
 
 
 
