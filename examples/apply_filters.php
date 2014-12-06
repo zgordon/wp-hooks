654bed8961c815a1823ff1a9e0d10ce4f4dcf673
Zac Gordon <email@zacgordon.com>
2014-12-06 14:53:30 -0500

    Updates to Filter Functions

--- examples/apply_filters.php
+++ examples/apply_filters.php
@@ -1,19 +1,15 @@
 <?php 
 
+// First create a function and hook it into new example hook
 function example_callback( $string, $arg1 ) {
-
-
 	return $string;
-
 }
 add_filter( 'example_filter', 'example_callback', 10, 2 );
 
+// Then use apply_filters in your code when you want the filter to run
+echo $value = apply_filters( 'example_filter', 'default_value', $string, $arg1 );
 
-
-echo $value = apply_filters( 'example_filter', 'default', $string, $arg1 );
-
-// http://wpseek.com/function/apply_filters/
-
+// From http://wpseek.com/function/apply_filters/
 ?>
 
 
