2966c80134466074b6bc203df2bb69161c51bffb
Zac Gordon <email@zacgordon.com>
2014-12-07 22:50:45 -0500

    Updating search code

--- examples/search_core.php
+++ examples/search_core.php
@@ -1,8 +1,6 @@
 // Search for functions that hook into a filter or action
-// Leave off closing mark
-// Search with and without spaces between ( and the '
 
-add_filter( 'wp_title // OR  without spaces add_filter('wp_title
+add_filter( 'wp_title
 add_action( 'init
 
 // See where filter or hook is applied
