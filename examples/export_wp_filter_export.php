2a44aeeecddb60a33ca7afef8400ca07ac87dc44
Zac Gordon <email@zacgordon.com>
2014-12-07 22:41:41 -0500

    Added search core slides

---
+++ examples/export_wp_filter_export.php
@@ -0,0 +1,29 @@
+'wp_title' =>
+  array (
+      10 =>
+          array (
+              'wptexturize' =>
+                  array (
+                      'function' => 'wptexturize',
+                      'accepted_args' => 1,
+                  ),
+              'convert_chars' =>
+                  array (
+                      'function' => 'convert_chars',
+                      'accepted_args' => 1,
+                  ),
+              'esc_html' =>
+                  array (
+                      'function' => 'esc_html',
+                      'accepted_args' => 1,
+                  ),
+          ),
+      11 =>
+          array (
+              'capital_P_dangit' =>
+                  array (
+                      'function' => 'capital_P_dangit',
+                      'accepted_args' => 1,
+                  ),
+          ),
+  )
---
+++ examples/search_core.php
@@ -0,0 +1,11 @@
+// Search for functions that hook into a filter or action
+// Leave off closing mark
+// Search with and without spaces between ( and the '
+
+add_filter( 'wp_title // OR  without spaces add_filter('wp_title
+add_action( 'init
+
+// See where filter or hook is applied
+
+apply_filters( 'wp_title
+do_action( 'init
